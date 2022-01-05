2f5b222454721e86604c336491348a54fdf81fe8
NyHagan <nanaohagan@gmail.com>
2022-01-05 20:03:24 +0000

    added parameter to reject numbers less than 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let button= document.getElementById("submit");
-
-})
 
 function calculateBMI(){
-    let height = parseInt(document.getElementById("height").value);
-    let weight = parseInt(document.getElementById("weight").value);
-    let result = document.getElementById("result");
+    const height = parseInt(document.getElementById("height").value);
+    const weight = parseInt(document.getElementById("weight").value);
+    const result = document.getElementById("result");
     
-
-    if (height === "" || isNaN(height)) {
-        result.innerHTML = "Please enter a valid height";
-    }else if(weight === "" || isNaN(weight)){
-        result.innerHTML = "Please enter a valid weight";
+    // check if entered height is a number, or empty or negative
+    if (height === "" || isNaN(height) || height < 0) {
+        result.innerHTML = "Please enter a valid positive height";
+    }else if(weight === "" || isNaN(weight)|| weight < 0){
+        result.innerHTML = "Please enter a valid positive weight";
     }else{
          bmi = (weight / ((height * height) 
         / 10000)).toFixed(2);
@@ -23,7 +19,8 @@ function calculateBMI(){
 }
 
 function displayInfo(){
-    let info= document.getElementById("info");
+    // This function displays the result to the info id element
+    const info= document.getElementById("info");
     if (bmi < 18.6) {
         info.innerHTML ="<p>You are UNDERWEIGHT</p>" ;
     }else if (bmi >= 18.6 && bmi < 24.9){
@@ -36,12 +33,12 @@ function displayInfo(){
 
 
 function displayReset(){
-    let again = document.getElementById("reload");
+    const again = document.getElementById("reload");
         again.style.visibility ="visible";
 }
 
 function changeText(){
-    let text = document.getElementById("text");
+    const text = document.getElementById("text");
         text.innerText = "Your Body Mass Index Is:";
 }
 
